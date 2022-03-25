903c2716794e80732ba57f1debac28ce22f3e0f2
Manny <manroven-singh@hotmail.com>
2022-03-25 21:13:16 +1030

    testing further if statements, if special characters not selected it will load error

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ var lengthOfPassword = prompt("how many characters would you like (must be betwe
 if (lengthOfPassword >= 8 && lengthOfPassword <= 128) {
   password.length = lengthOfPassword;
 }
- else { alert("please select pword between 8-128");  alert("error-cannot create pword"); }
-
-
+ else { 
+   alert("please select pword between 8-128");
+ }
 
 var specialCharacters = confirm("Click ok to confirm if you want special characters");
 
@@ -32,6 +32,15 @@ var numericalcharacters = confirm("Click ok to confirm if you want numbers");
 var lowerCaseCharacter = confirm("Click ok to confirm if you want lowercase letters");
 
 var upperCaseCharacter = confirm("Click ok to confirm if you want uppercase letters");
+
+// selecting ok= true which will trigger e.g. lowercase be adding to password characters.
+if (specialCharacters === true) {
+  passwordCharacters += specialCharacterss
+}
+else {
+  alert("error-cannot create pword");
+}
+
 
 
 
