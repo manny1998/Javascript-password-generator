698b14aedceec99c042d34ee1c67be702e98d16d
Manny <manroven-singh@hotmail.com>
2022-03-25 20:42:34 +1030

    adding constants and creating if statement test for password length

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
 // This variable targets the generate button
 var generateBtn = document.querySelector("#generate");
 
+//Constant variable for the special characters, numberss,etc used for generating password.
+const specialCharacterss = "!@#$%^&*()_-+={}[|\];:''<>,.?/";
+const numberss = "1234567890";
+const upperCaseLetters = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+const lowerCaseLetters = "qwertyuioplkjhgfdaszmxncbv";
+const lengthOfPassword= "";
+
+
 
 function generatePassword() {
+  userPassword = "";
+  passwordCharacters = "";
 
 //creating pop ups with password requirements
 var lengthOfPassword = prompt("how many characters would you like (must be between 8-128)");
 
+//
+if (lengthOfPassword >= 8 && pwLength <= 128) {
+  password.length = lengthOfPassword;
+}
+ else { alert("please select pword between 8-128");  alert("error-cannot create pword"); }
+
+
+
 var specialCharacters = confirm("Click ok to confirm if you want special characters");
 
-var numericalcharacters = confirm("Click ok to confirm if you want numbers")
+var numericalcharacters = confirm("Click ok to confirm if you want numbers");
 
-var lowerCaseCharacter = confirm("Click ok to confirm if you want lowercase letters")
+var lowerCaseCharacter = confirm("Click ok to confirm if you want lowercase letters");
 
-var upperCaseCharacter = confirm("Click ok to confirm if you want uppercase letters")
+var upperCaseCharacter = confirm("Click ok to confirm if you want uppercase letters");
 
 
 
-  return("generate password");
+return userPassword; 
 }
 
-//
+
+
+
+ 
 
 
 // Write password to the #password input
@@ -31,6 +52,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 //click generate button and it calls write password function
 generateBtn.addEventListener("click", writePassword);
